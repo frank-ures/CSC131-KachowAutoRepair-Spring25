5f5e764ba221f67b224b6d30b96c215d6bddc40b
Klayton Hutchins <klaytonhutchins@gmail.com>
2025-03-26 14:11:19 -0700

    SCRUM-84 : change appt type object to reflect figma services page

--- Backend/models/appointment_type.js
+++ Backend/models/appointment_type.js
@@ -3,8 +3,10 @@ import mongoose from 'mongoose';
 const AppointmentTypeSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
   name: {type: String, required: true},
-  price: {type: Number, required: true},
-  estimatedTimeInMin: {type: Number, required: true}
+  priceMin: {type: Number, required: true},
+  priceMax: {type: Number, required: true},
+  timeMin: {type: Number, required: true},
+  timeMax: {type: Number, required: true}
 });
 
 export default mongoose.model('appointment_types', AppointmentTypeSchema);
