4f29df9afb9129d967fec3dcb11b008fe9b1661e
Klayton Hutchins <klaytonhutchins@gmail.com>
2025-04-23 10:02:14 -0700

    SCRUM-138 : Create model for clocking in and out

---
+++ Backend/models/clock_in_out.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const ClockInOutSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+    isClockIN: { type: Boolean, required: true },
+    clockTime: { type: String, required: true }
+});
+
+export default mongoose.model('clock_in_out', ClockInOutSchema);
